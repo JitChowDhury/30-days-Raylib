97ca8b9a4f27c440efa71c84a53489941bfa099f
JitChowDhury <jitchowdhury8717@gmail.com>
2025-12-06 12:15:12 +0530

    Refactored The code

--- day10/main.cpp
+++ day10/main.cpp
@@ -9,82 +9,77 @@ enum State
 
 struct Enemy
 {
+  // patrol points
   Vector2 pointA{100, 100};
-  Vector2 pointB{500, 100};
-  Vector2 enemyPosition = {pointA.x, pointA.y};
+  Vector2 pointB{500, 500};
+  // Enemy Datas
+  Vector2 position{pointA.x, pointA.y};
   float size{20.f};
   float speed{100.f};
-  float actualSpeed{100.f};
-  Rectangle rec{enemyPosition.x, enemyPosition.y, size, size};
+  float baseSpeed{speed};
+
+  // patrol behaviour
+  int direction{+1};
+  float idleDuration{2.f};
+  int idleTimer{};
+
   float chaseRange{100.f};
-  int patrolDirection{+1};
-  float idleTimer = 2.f;
-  float timer{0.f};
 
   State state{PATROL};
 
   void Update(Vector2 playerPos)
   {
+    float dt = GetFrameTime();
     // if distance to player is less than the chaseRange enemy will chase the player
 
-    float distance = Vector2Distance(enemyPosition, playerPos);
+    float distance = Vector2Distance(position, playerPos);
 
-    if (distance < chaseRange)
-    {
-      state = CHASE;
-    }
-    else
-    {
-      state = PATROL;
-    }
+    state = (distance < chaseRange) ? CHASE : PATROL;
 
     switch (state)
     {
     case PATROL:
-      enemyPosition.x += patrolDirection * speed * GetFrameTime();
-      if (enemyPosition.x >= pointB.x)
-      {
-        if (timer > idleTimer)
-        {
-          patrolDirection = -1;
-          timer = 0;
-          speed = actualSpeed;
-        }
-        else
-        {
-          timer += GetFrameTime();
-          speed = 0.f;
-        }
-      }
-      else if (enemyPosition.x <= pointA.x)
-      {
-        if (timer > idleTimer)
-        {
-          patrolDirection = +1;
-          timer = 0;
-          speed = actualSpeed;
-        }
-        else
-        {
-          timer += GetFrameTime();
-          speed = 0.f;
-        }
-      }
-
+      Patrol(dt);
       break;
-
     case CHASE:
-      Vector2 direction = Vector2Subtract(playerPos, enemyPosition);
-      direction = Vector2Normalize(direction);
-
-      enemyPosition.x += direction.x * speed * GetFrameTime();
-      enemyPosition.y += direction.y * speed * GetFrameTime();
-
+      Chase(playerPos, dt);
       break;
+    };
+  }
+
+  void Patrol(float dt)
+  {
+    position.x += direction * speed * dt;
+
+    if (position.x >= pointB.x)
+    {
+      HandleIdleState(-1, dt);
     }
   }
-};
 
+  void HandleIdleState(int newDirection, float dt)
+  {
+
+    if (idleTimer >= idleDuration)
+    {
+      direction = newDirection;
+      speed = baseSpeed;
+      idleTimer = 0.f;
+    }
+    else
+    {
+      idleTimer += dt;
+      speed = 0.f;
+    }
+  }
+
+  void Chase(Vector2 playerPos, float dt)
+  {
+    Vector2 direction = Vector2Normalize(Vector2Subtract(playerPos, position));
+    position.x += direction.x * speed * dt;
+    position.y += direction.y * speed * dt;
+  }
+};
 void HandleInput(Vector2 &playerPos, float speed, float playerSize)
 {
   Vector2 velocity{};
@@ -106,7 +101,8 @@ void HandleInput(Vector2 &playerPos, float speed, float playerSize)
     velocity.x += speed * GetFrameTime();
   }
 
-  playerPos += velocity;
+  // playerPos += velocity
+  playerPos = Vector2Add(playerPos, velocity);
 
   playerPos.x = Clamp(playerPos.x, 0, GetScreenWidth() - playerSize);
   playerPos.y = Clamp(playerPos.y, 0, GetScreenHeight() - playerSize);
@@ -121,23 +117,23 @@ int main()
 
   //-----------------PLAYER--------------
 
-  float speed{200.f};
+  float playerSpeed{200.f};
   float playerSize{40.f};
   Vector2 playerPos{WINDOW_WIDTH / 2 - playerSize, WINDOW_HEIGHT / 2 - playerSize};
 
-  Enemy enemy{20, 100.f};
+  Enemy enemy;
 
   while (!WindowShouldClose())
   {
 
-    HandleInput(playerPos, speed, playerSize);
+    HandleInput(playerPos, playerSpeed, playerSize);
     enemy.Update(playerPos);
 
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    DrawRectangle(playerPos.x, playerPos.y, playerSize, playerSize, ORANGE);
-    DrawRectangle(enemy.enemyPosition.x, enemy.enemyPosition.y, enemy.size, enemy.size, RED);
+    DrawRectangleV(playerPos, {playerSize, playerSize}, ORANGE);
+    DrawRectangleV(enemy.position, {enemy.size, enemy.size}, RED);
 
     EndDrawing();
   }
